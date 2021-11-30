bfe2aca887e5b8aa273dbf04f7be6dc0d6696114
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2021-11-30 11:49:29 +0100

    CHNGD: budget row subtotals are activated as multiple choice value changes are computed in MultipleChoiceCells compo

--- src/pages/layout/appBody/budget/rows/rowCells/MultipleChoiceCells.js
+++ src/pages/layout/appBody/budget/rows/rowCells/MultipleChoiceCells.js
@@ -1,20 +1,37 @@
 import { accounting } from "accounting";
 import { TableCell } from "@mui/material";
 import MultipleChoice from "../../multipleChoice/MultipleChoice";
+import { useState, useEffect } from "react";
 
 const MultipleChoiceCells = ({ pax, cat, id, options, date }) => {
+  const [value, setValue] = useState(options[0].name);
+  const [selected, setSelected] = useState({});
+
+  useEffect(() => {
+    const selectedOption = options.find((option) => option.name === value);
+    setSelected(selectedOption);
+  }, [value]);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
     <>
       <TableCell>{`${cat} options`}</TableCell>
       <TableCell>
-        <MultipleChoice options={options} date={date} id={id} />
+        <MultipleChoice
+          options={options}
+          date={date}
+          id={id}
+          selectedOption={selected}
+          value={value}
+          handleChange={handleChange}
+        />
       </TableCell>
       <TableCell>{pax}</TableCell>
+      <TableCell>{accounting.formatMoney(selected["price"], "€")}</TableCell>
       <TableCell>
-        {/*   {accounting.formatMoney(selectedOption["price"], "€")} */}
-      </TableCell>
-      <TableCell>
-        {/*     {accounting.formatMoney(pax * selectedOption["price"], "€")} */}
+        {accounting.formatMoney(pax * selected["price"], "€")}
       </TableCell>
     </>
   );
