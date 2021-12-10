975212d0007f47c1d46d9a8f6cb3b9e9d5b491bb
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2021-12-10 19:25:59 +0100

    ADD: Markers to the Map

--- src/pages/layout/Header/headerElements/map/mapContent/Map.js
+++ src/pages/layout/Header/headerElements/map/mapContent/Map.js
@@ -1,12 +1,20 @@
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { useState } from "react";
+import getCenter from "geolib/es/getCenter";
+import { Icon } from "@iconify/react";
 
 const Map = () => {
+  const coords = [
+    { latitude: 41.368679, longitude: 2.189983 },
+    { latitude: 41.394591, longitude: 2.157261 },
+    { latitude: 41.389121, longitude: 2.162645 },
+  ];
+  const center = getCenter(coords);
   const [viewport, setViewport] = useState({
     width: "100%",
     height: "100%",
-    latitude: 37.7577,
-    longitude: -122.4376,
+    latitude: center.latitude,
+    longitude: center.longitude,
     zoom: 11,
   });
 
@@ -15,7 +23,21 @@ const Map = () => {
       mapStyle='mapbox://styles/oliverm316/ckx0n8rqw24sr15o5vei79sto'
       mapboxApiAccessToken='pk.eyJ1Ijoib2xpdmVybTMxNiIsImEiOiJja3gwbzQ5dWMwdGY5MnFvYmJoZWFqYmg5In0.DHlbmXqmGZjEirgSs29hUA'
       {...viewport}
-    ></ReactMapGL>
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {coords.map((coord) => (
+        <div key={coord.latitude}>
+          <Marker
+            latitude={coord.latitude}
+            longitude={coord.longitude}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <Icon icon='bx:bx-hotel' color='#ea5933' width='40' />
+          </Marker>
+        </div>
+      ))}
+    </ReactMapGL>
   );
 };
 
